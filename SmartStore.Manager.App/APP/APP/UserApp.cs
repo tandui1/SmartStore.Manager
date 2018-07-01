902073d474be21753b85d86ef2d245af5038aa2b
lin chen <1622178647@qq.com>
2018-07-01 21:05:44 +0800

    框架

---
+++ SmartStore.Manager.App/APP/APP/UserApp.cs
@@ -0,0 +1,23 @@
+﻿using SmartStore.Manager.App.APP.IAPP;
+using SmartStore.Manager.Domain.Service.IService;
+using SmartStore.Manager.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStore.Manager.App.APP.APP
+{
+   public class UserApp: IUserApp
+    {
+        private readonly IUserService _userService;
+        public UserApp(IUserService userService) {
+            this._userService = userService;
+        }
+        public UserDto GetUserByName(string name)
+        {
+           return  this._userService.GetUserByName(name);
+        }
+    }
+}
---
+++ SmartStore.Manager.App/APP/IAPP/IUserApp.cs
@@ -0,0 +1,14 @@
+﻿using SmartStore.Manager.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartStore.Manager.App.APP.IAPP
+{
+  public  interface IUserApp
+    {
+        UserDto GetUserByName(string name);
+    }
+}
